556abc291a3d0e41b92bdbb08a25a0db18b055f9
hybridNeo <rahul.mahadev7@gmail.com>
2014-09-15 14:27:47 +0530

    asas

---
+++ w2p2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class w2p2{
+	static int a =100;
+	static class StaticNested{
+				public void footer(){
+			System.out.println(a);
+			}
+	}
+	public static void main(String[] args){
+		System.out.println("Hello");
+		
+	  class NestedClass{
+			public String a = "Hello";
+		
+			public void foo(){
+				System.out.println("Booo\n");
+			}
+		}
+		
+		NestedClass n = new NestedClass();
+		n.foo();
+		StaticNested s = new StaticNested();
+		s.footer();	
+	}
+	
+}
